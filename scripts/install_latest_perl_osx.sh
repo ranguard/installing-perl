3687ba2dc7663bab551d3cf106c3a2cd7a0a58cd
Torsten Raudssus <github@raudssus.de>
2011-05-21 21:47:19 +0200

    Fixing the shell script (hups)

--- scripts/install_latest_perl_osx.sh
+++ scripts/install_latest_perl_osx.sh
@@ -2,29 +2,32 @@
 
 INSTALLER_PERL_VERSION=5.14.0
 
-BASHP="~/.bash_profile"
-BASHR="~/.bashrc"
+BASHP=~/.bash_profile
+BASHR=~/.bashrc
 
 echo -n "Checking for .bash_profile... "
 if [ -f $BASHP ]; then
 	echo "FOUND"
 	echo -n "Checking for .bashrc sourcing inside of .bash_profile... "
-	if [ cat $BASHP | grep "source " | grep "bashrc" ]; then
+	if [ "" != "$( cat $BASHP | grep 'source ' | grep 'bashrc' )" ]; then
 		echo "FOUND"
 	else
 		echo "NOT FOUND"
 		echo "Adding 'source $BASHR' to $BASHP"
-		echo "# Added by install_latest_perl_osx.pl\n[ -r $BASHR ] && source $BASHR" >> $BASHP
+		echo "# Added by install_latest_perl_osx.pl" >>$BASHP
+		echo "[ -r $BASHR ] && source $BASHR" >>$BASHP
 	fi
 else
 	echo "NOT FOUND"
 fi
 
+exit
+
 echo "Installing perlbrew"
 curl -L http://xrl.us/perlbrewinstall | bash
 
 echo "Updating $bashrc with perlbrew command"
-cat ~/perl5/perlbrew/etc/bashrc >> ~/.bashrc
+cat ~/perl5/perlbrew/etc/bashrc >>$BASHR
 
 echo "Updating your current environment"
 source ~/perl5/perlbrew/etc/bashrc
